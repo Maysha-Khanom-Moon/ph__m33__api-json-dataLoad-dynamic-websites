ebd634f7f941870ea38596b686f5a7cb10d2ce2d
Maysha-Khanom-Moon <maysha412@gmail.com>
2024-02-29 23:41:36 +0600

    all about json structure, json parse, stringify

--- js/json.js
+++ js/json.js
@@ -1,11 +1,15 @@
 /**
  * ---------- javaScript object notation (json) ----------
  * 
- * ==> JSON.stringify --> converts a js value to a json string
+ * ==> JSON.stringify --> converts a js object to a json string
+ *      ==> for number, null, undefined, true, false there is no "". otherwise all are wrap via ""
+ * 
+ * ==> JSON.parse --> converts a json string to original js object
  */
 
 const user = {id: 1, naem: 'Gorib Aamir', job: 'actor'};
 const stringified = JSON.stringify(user);
+const userObj = JSON.parse(stringified);
 
 console.log(user);
 // { id: 1, naem: 'Gorib Aamir', job: 'actor' }
@@ -13,4 +17,26 @@ console.log(user);
 console.log(stringified);
 // {"id":1,"naem":"Gorib Aamir","job":"actor"}
 
+console.log(userObj);
+// { id: 1, naem: 'Gorib Aamir', job: 'actor' }
+
+
+// again
+const shop = {
+    owner: 'Alia',
+    address: {
+        street: 'kochukhet voot er goli',
+        city: 'ranbir',
+        country: 'BD'
+    },
+    products: ['laptop', 'mic', 'monitor', 'keyboard'],
+    revenue: 45000,
+    isOpen: true,
+    isNew: false
+}
+
+const jsonShop = JSON.stringify(shop);
+
+console.log(shop);
+console.log(jsonShop);
 
